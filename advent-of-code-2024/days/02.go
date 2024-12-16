b94d9ceb197dfbfb0c4e0abd000a9f329158380f
arash-shafiee <i.arash.shafiee@gmail.com>
2024-12-16 12:13:18 +0330

    aoc-2024: day 2 part2

--- advent-of-code-2024/days/02.go
+++ advent-of-code-2024/days/02.go
@@ -10,7 +10,7 @@ import (
 )
 
 func D2p1() {
-	file, err := os.Open("./inputs/02-example.txt")
+	file, err := os.Open("./inputs/02.txt")
 
 	if err != nil {
 		fmt.Println(err)
@@ -25,24 +25,8 @@ func D2p1() {
 	for scanner.Scan() {
 		txt := scanner.Text()
 		nums := strings.Split(txt, " ")
-		a, _ := strconv.Atoi(nums[0])
-		b, _ := strconv.Atoi(nums[1])
 
-		asc := a-b < 0
-
-		is_safe := true
-
-		for i := 0; i < len(nums)-1; i++ {
-			n1, _ := strconv.Atoi(nums[i])
-			n2, _ := strconv.Atoi(nums[i+1])
-
-			diff := n1 - n2
-
-			if diff < 0 != asc || math.Abs(float64(diff)) < 1 || math.Abs(float64(diff)) > 3 {
-				is_safe = false
-				break
-			}
-		}
+		is_safe := check_1(nums)
 
 		if is_safe {
 			safe_count += 1
@@ -66,7 +50,55 @@ func D2p2() {
 
 	scanner := bufio.NewScanner(file)
 
+	safe_count := 0
+
+	for scanner.Scan() {
+		txt := scanner.Text()
+		nums := strings.Split(txt, " ")
+
+		if check_rep(nums) {
+			safe_count += 1
+		}
+	}
+
+	fmt.Println(safe_count)
+
 	if err := scanner.Err(); err != nil {
 		fmt.Println(err)
 	}
+}
+
+func check_1(nums []string) bool {
+	is_safe := true
+	a, _ := strconv.Atoi(nums[0])
+	b, _ := strconv.Atoi(nums[1])
+
+	asc := a-b < 0
+
+	for i := 0; i < len(nums)-1; i++ {
+		n1, _ := strconv.Atoi(nums[i])
+		n2, _ := strconv.Atoi(nums[i+1])
+
+		diff := n1 - n2
+
+		if diff < 0 != asc || math.Abs(float64(diff)) < 1 || math.Abs(float64(diff)) > 3 {
+			is_safe = false
+			break
+		}
+	}
+	return is_safe
+}
+
+// PERF: don't use brute force method
+func check_rep(nums []string) bool {
+	for i := range nums {
+		arr := append([]string{}, nums[0:i]...)
+		arr = append(arr, nums[i+1:]...)
+
+		if check_1(arr) {
+			return true
+		}
+	}
+
+	return false
 }
